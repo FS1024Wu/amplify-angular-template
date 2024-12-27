6727f73a673296cf05560ff16509e102a10a2231
wfs <carlowu.work@gmail.com>
2024-12-27 00:32:08 -0500

    add sign out

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -15,6 +15,11 @@ const client = generateClient<Schema>();
 export class TodosComponent implements OnInit {
   todos: any[] = [];
 
+
+  deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   ngOnInit(): void {
     this.listTodos();
   }
